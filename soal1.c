55bf4ddb7bf595fc305f31a52106adc269833a1c
chrisjo-zzz <jonathanhutajulu94@gmail.com>
2026-05-05 08:29:00 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
+#include <string.h>
+#define MAX 100
 
-int main(void) {
+int main (){
+    char input[MAX];
+    char stack[MAX];
+    int top = -1;
+    scanf("%s", input);
+    if (input == "a"||"b"||"c"||"d"||"e"||"f"||"g"||"h"||"j"||"k"||"l"||"m"||"n"||
+    "o"||"p"||"q"||"r"||"s"||"t"||"u"||"v"||"w"||"x"||"y"||"z"||
+    "(" && ")" || input == "[" && "]" || input == "{" && "}"){
+        printf ("VALID");
+    } else {
+        printf ("INVALID");
+    }
     return 0;
 }
